243cf07ce093de000faf7ca4f9c2d094827e3db9
Marcello Marchetti <mamarche@outlook.it>
2016-01-07 11:31:52 +0100

    First commit

---
+++ ExcelAddInDemoWeb/AddIn/Home/Home.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#crea-tabella').click(creaTabella);
+        });
+    };
+
+    function creaTabella() {
+
+        //creo un oggetto tabella
+        var miaTabella = new Office.TableData();
+        //aggiungo le intestazioni di colonna
+        miaTabella.headers = ['Nome', 'Cognome'];
+        //Aggiungo un array di righe con i dati di test
+        miaTabella.rows = [['Mario', 'Rossi'], ['Franco', 'Bianchi'], ['Luca', 'Verdi']];
+
+        //utilizzo l'API per generare la tabella nel foglio
+        Office.context.document.setSelectedDataAsync(miaTabella, { coercionType: Office.CoercionType.Table },
+            function (result) {
+                var error = result.error
+                if (result.status === Office.AsyncResultStatus.Failed) {
+                    $('#error-log').text(error.name + ": " + error.message);
+                }
+            });
+    }
+    
+})();
